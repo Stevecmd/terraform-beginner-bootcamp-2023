80457000d1b53237701d72296cf7b15a3f3c5c0f
Steve Murimi <murimi101@gmail.com>
2023-10-16 10:50:43 +0000

    Hail Mary test!

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -20,7 +20,7 @@ variable "bucket_name" {
   }
 }
 
-/*
+
 variable "index_html_filepath" {
   description = "The file path for index.html"
   type        = string
@@ -40,4 +40,3 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
-*/
